e3be47f173ca583e038118c1aec327cdb30239b2
Yoanmtl514 <152190421+Yoanmtl514@users.noreply.github.com>
2024-02-07 18:22:38 -0500

    Update configuration.h

--- configuration.h
+++ configuration.h
@@ -1,1 +1,27 @@
+/****************************************************************************************
+/*                                  CONFIGURATION.H                                     *
+/****************************************************************************************
+    Auteur : Yoan Sapet & Alexis St-Pierre
+    Date   : 24 Janvier 2024
 
+    Ce module contient les options de configuration pour la démonstration du cours03.
+    Changez les valeurs des deux define pour passer d'un mode applicatif à un mode de
+    tests.
+
+    Liste des fonctions publiques:
+      - Aucunes.
+
+*****************************************************************************************/
+
+
+#ifndef __H2024TP1_CONFIGURATION__
+#define __H2024TP1_CONFIGURATION__
+
+#define APPLICATION     0
+#define TEST            1
+#define PORT55001       0
+#define PAIRE           0
+#define IMPAIRE         0
+#define MODEPARITE      0
+
+#endif
