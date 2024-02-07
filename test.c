45ce59330b7cdf73cd87122eb21dc33d846f0dbc
Yoanmtl514 <152190421+Yoanmtl514@users.noreply.github.com>
2024-02-07 18:23:42 -0500

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,50 @@
 
+/****************************************************************************************
+/*                                      TEST                                           *
+/****************************************************************************************
+Auteur : Yoan Sapet & Alexis St-Pierre
+Date   : 24 Janvier 2024
+
+Fonction publique:
+
+Ce module permet de lancer tous les tests unitaires de chaque module de l'application
+*****************************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "configuration.h"
+
+
+#if TEST
+
+/*
+    AFFICHER_ENTETE_TEST
+
+    Cette fonction permet d'afficher une entete quand le mode test est lance
+
+    Paramètres:
+        - Aucun
+
+    Retour:
+        - Aucun.
+*/
+static void afficher_entete_test();
+
+int main(void)
+{
+    afficher_entete_test();
+    return EXIT_SUCCESS;
+}
+
+//
+// On affiche l'entete du mode test
+//
+static void afficher_entete_test()
+{
+    printf("Pogramme execute en mode test\n");
+    printf("Yoan Sapet & Alexis St-Pierre\n");
+    printf("Session: Hiver 2024\n");
+
+}
+
+#endif
