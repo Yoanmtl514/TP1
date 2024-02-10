2dd150975b791649ba51feac1647928a0903d711
Yoanmtl514 <152190421+Yoanmtl514@users.noreply.github.com>
2024-02-10 17:58:49 -0500

    Update utilitaire.c

--- utilitaire.c
+++ utilitaire.c
@@ -1,61 +1,99 @@
 /****************************************************************************************
-/*                                      utilitaire.h                                       *
+/*                                      UTILITAIRE.C                                    *
 /****************************************************************************************
-    Auteur : Yoan Sapet & Alexis St-Pierre
-    Date   : 24 Janvier 2024
+Auteur : Yoan Sapet & Alexis St-Pierre
+Date   : 24 Janvier 2024
 
+Fonction publique:
 
-    Liste des fonctions publiques:
-
-
+Ce module contient les fonctions utilitaires du projet
 *****************************************************************************************/
-#ifndef __H2024TP1_utilitaire.h__
-#define __H2024TP1_utilitaire.h__
 
-/*
-    AFFICHER_HEURE
+#include <stdlib.h>
+#include <stdio.h>
 
-    Cette fonction permet d'afficher une heure au format 00h00m00s a partir d'un temps
-    en secondes
+#include "utilitaire.h"
 
-    Paramètres:
-        - Un entier  non signe representant le nombres de secondes a convertir
 
-    Retour:
-        - Aucun.
+//
+// Converti des secondes en heures et affiche l'heure au format 00h00m00s
+//
+void afficher_heure(unsigned int entier)
+{
+    int h;
+    int m;
+    int s;
 
-*/
-void afficher_heure(unsigned int entier);
+    h = (entier /3600);
+    if( h<10)
+    {
+        printf("0%dh",h);
+    }
+    m = (entier -(3600*h))/60;
+    if(m<10)
+    {
+        printf("0%dm",m);
+    }
+    s = (entier -(3600*h)-(m*60));
+    if(s < 10)
+    {
+        printf("0%ds",s);
+    }
+    else
+    {
+        printf("%dh%dm%ds\n",h,m,s);
+    }
+}
 
-/*
-    DECIMALE_A_OCTALE
+//
+// Converti un entier non signe en base 10 en entier non signer en base 8
+//
+int decimale_a_octale (unsigned int entier_decimal)
+{
+    int entier_octal = 0;
+    int i = 1;
 
-    Cette fonction permet de convertir un entier non signe en base 10 en entier non signe de base 8
+    while (entier_decimal != 0) {
+        entier_octal = entier_octal +  (entier_decimal % 8) * i;
+        entier_decimal = entier_decimal / 8;
+        i = i*10;
+    }
 
-    Paramètres:
-        - Un entier non signe en decimal
+    return entier_octal;
+}
 
-    Retour:
-        - Un entier non signe en octal.
+//
+// Calcul le nombre de bits actifs d'un entier donne
+//
+int calculer_nb_bits_actifs( unsigned int valeur)
+{
+    int compteur=0;
 
-*/
-int decimale_a_octale (unsigned int entier_decimal);
+    while ( valeur != 0)
+    {
+       if (valeur%2 ==1)
+       {
+           compteur++;
+       }
 
-/*
-    CALCULER_NB_BITS_ACTIFS
+       valeur = valeur/2;
 
-    Cette fonction permet de compter le nombre de bits actifs (bit ayant une valeur de 1) a partir d'un
-    entier non signe.
+    }
 
-    Paramètres:
-        - Un entier non signe
+    return compteur;
 
-    Retour:
-        - Un entier non signe representant le nombre de bits actifs dans la valeur recu en parametre.
+}
 
-*/
-int calculer_nb_bits_actifs( unsigned int valeur);
+void afficher_bits(unsigned int valeur, int bit_depart, int bit_fin) {
 
-void afficher_bits ( unsigned int valeur, int bit_faible, int bit_fort);
+    printf("(0x%08X) : ", valeur);
 
-void afficher_mot_a429(unsigned int mot);
+    for (int i = bit_fin; i >= bit_depart; i--) {
+        printf("%d", (valeur >> i) & 1);
+
+        //Pour afficher des groupes de 4 bits,
+        if ((i - bit_depart) % 4 == 0 && i != bit_depart)
+            printf(" ");
+    }
+
+}
