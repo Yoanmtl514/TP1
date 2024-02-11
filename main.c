2879cc7103e660c75bbd0c5bcea041311385937c
Yoanmtl514 <152190421+Yoanmtl514@users.noreply.github.com>
2024-02-11 12:18:41 -0500

    Update main.c

--- main.c
+++ main.c
@@ -12,8 +12,9 @@ leur 4 composantes cohérentes l'une par rapport à l'autre.
 #include <stdio.h>
 
 #include "configuration.h"
-#include "decodeur/a429.h"
-#include "udp/port_udp.h"
+
+#include "C:/Users/Yoan/Desktop/INF147/H2024TP1/decodeur/a429.h"
+#include "udp\port_udp.h"
 
 /****************************************************************************************
 *                           DÉFINTION DES TYPES ET CONSTANTES                           *
@@ -29,18 +30,24 @@ leur 4 composantes cohérentes l'une par rapport à l'autre.
 #if APPLICATION
 int main(void)
 {
-
+    //Requis 26 : afficher l'entete du decodeur
     afficher_entete_decodeur();
 
-    initialiser_port_udp(55001);
+    //Requis 27 initialiser le port UDP 55001
+    int id_port_udp = initialiser_port_udp(55001);
 
-    
-    while (1) {
+    //Requis 28: Lecture continue sur le port udp 5501
 
+    while(1)
+    {
+        int mot_arinc429;
+        // Lecture sur le port UDP 5501
+        recevoir_port_udp(id_port_udp, &mot_arinc429, sizeof(unsigned int));
 
-        }
+        // Requis 29 : Afficher les mots ARINC-429 reçus
+        afficher_mot_a429(mot_arinc429);
     }
-    return EXIT_SUCCESS;
+
 }
 
 
